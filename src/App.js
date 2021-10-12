167e4e38e4c5f60abfecd6445a208bb073b903c7
EjilolaHammedEjitomiwa <geotechbrain@gmail.com>
2021-10-12 22:28:39 +0100

    Left side bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <div className="app_body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
+
+function Sidebar() {
+  const recentItems = (topic) => {
+    return (
+      <div className="siderbar_recentItem">
+        <span className="sidebar_hash">#</span>
+        <p>{topic}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4E1BAQF6MHpEwyuTpg/company-background_10000/0/1559664567142?e=2159024400&v=beta&t=VZX1oqx_DJAFtkDkPoMHH60OF-MIwWjyP7VaQ43weNA"
+          alt=""
+        />
+        <Avatar className="sidebar_avater" />
+        <h2>Ejilola Hammed</h2>
+        <h4>geotechbrain@gmail.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebarstat_counter">2,543</p>
+        </div>
+
+        <div className="sidebar_stat">
+          <p>Views on Post</p>
+          <p className="sidebarstat_counter">2,800</p>
+        </div>
+      </div>
+
+      <div className="sidebar_button">
+        <p>Recent</p>
+        {recentItems("reactjs")}
+        {recentItems("programing")}
+        {recentItems("software")}
+        {recentItems("design")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
